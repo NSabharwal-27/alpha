f6deb68588b7530206f55b699e2262ebf0d48750
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2026-08-08 10:25:57 -0400

    boot.s: adding the exception level detection code
    I think this works??

--- kernel/arch/aarch64/boot.s
+++ kernel/arch/aarch64/boot.s
@@ -1,6 +1,13 @@
 .global _start
 _start:
-	ldr x9, =stack_top
-	mov sp, x9
-	bl kmain
-	b .
+
+	// Sort out the Exception Level first.
+	mrs	x0, CurrentEL
+	ubfx	x0, x1, #2, #2
+
+	// Now, setup the stack.
+	ldr	x9, =stack_top
+	mov	sp, x9
+
+	bl	kmain
+	b	.
