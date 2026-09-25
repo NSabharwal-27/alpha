a10193d5426f6be12a9470cf7fe78963bca85505
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2026-09-25 08:28:32 -0400

    linker: re-wrote part of the linker, added .text.boot and a .bss section that was better defined. Makefile: comments were adjusted. boot.s: changed the name of stack_top to _stack_top.

--- kernel/arch/aarch64/boot.s
+++ kernel/arch/aarch64/boot.s
@@ -1,13 +1,14 @@
 .global _start
 _start:
 
-	// Sort out the Exception Level first.
+	/* Sort out the Exception Level first. */
 	mrs	x0, CurrentEL
 	ubfx	x0, x1, #2, #2
 
-	// Now, setup the stack.
-	ldr	x9, =stack_top
+	/* Now, setup the stack. */
+	ldr	x9, =_stack_top
 	mov	sp, x9
 
+	/* Now that's done, go to kernel */
 	bl	kmain
 	b	.
