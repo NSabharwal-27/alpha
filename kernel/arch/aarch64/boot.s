444da8c4db11e40e27ec5f1bb64af52106f3c22a
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2026-09-26 10:10:04 -0400

    boot.s: DO NOT INTERLEAVE TABS LIKE THAT

--- kernel/arch/aarch64/boot.s
+++ kernel/arch/aarch64/boot.s
@@ -3,30 +3,31 @@
 _start:
 
 	/* Sort out the Exception Level first. */
-	mrs	x0, CurrentEL
-	lsr	x0, x0, #2
-	cmp	x0, #1
-	beq	.EL1_INITIALIZE
+	mrs     x0, CurrentEL
+	lsr     x0, x0, #2
+	cmp     x0, #1
+	beq     .EL1_INITIALIZE
 
 
 .EL1_INITIALIZE:
 	/* Now, setup the stack. */
-	ldr	x9, =_stack_top
-	mov	sp, x9
-	ldr	x1, =__bss_start
-	ldr	x2, =__bss_end
+	ldr     x9, =_stack_top
+	mov     sp, x9
+	ldr     x1, =__bss_start
+	ldr     x2, =__bss_end
+	b       .bss_clear
 
 /* 
  * We are going to do 8-byte stores, so we can get that
  * 8-byte alignment, from linker.
  */
 .bss_clear:
-	cmp	x1, x2
-	beq	.CALL_KMAIN
-	str	xzr, [x1], #8
-	b	.bss_clear
+	cmp     x1, x2
+	bge     .CALL_KMAIN
+	str     xzr, [x1], #8
+	b       .bss_clear
 
 .CALL_KMAIN:
 	/* Now that's done, go to kernel */
-	bl	kmain
-	b	.
+	bl      kmain
+	b       .
