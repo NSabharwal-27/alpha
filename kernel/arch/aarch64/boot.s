cbbd7098690335e4df2b6d6c6b878380fe5bc70b
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2026-09-26 09:48:14 -0400

    boot/linker: now clear the bss section and guarantee 8-byte alignment. Also the stack has a reserved 16KB space.

--- kernel/arch/aarch64/boot.s
+++ kernel/arch/aarch64/boot.s
@@ -13,7 +13,20 @@ _start:
 	/* Now, setup the stack. */
 	ldr	x9, =_stack_top
 	mov	sp, x9
+	ldr	x1, =__bss_start
+	ldr	x2, =__bss_end
 
+/* 
+ * We are going to do 8-byte stores, so we can get that
+ * 8-byte alignment, from linker.
+ */
+.bss_clear:
+	cmp	x1, x2
+	beq	.CALL_KMAIN
+	str	xzr, [x1], #8
+	b	.bss_clear
+
+.CALL_KMAIN:
 	/* Now that's done, go to kernel */
 	bl	kmain
 	b	.
